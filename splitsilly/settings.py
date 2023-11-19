a7c936aefe7cb91499a03351d1d68a76377dfe9c
Patrick Gingras <775.pg.12@gmail.com>
2023-11-19 16:13:08 -0500

    settings and env stuff

--- splitsilly/settings.py
+++ splitsilly/settings.py
@@ -10,23 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENV = os.getenv("ENV")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+DEBUG = ENV == "dev"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$%kf+q*&^(x*+tswqo61xo5t57=k)s%8h@*1^gbill(c7-^&jh'
+ALLOWED_HOSTS = [os.getenv("DJANGO_HOST")]
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = ENV != "dev"
 
 # Application definition
 
