a1dd0cdb9940126f54842a92b2a166364aba317c
Patrick Gingras <775.pg.12@gmail.com>
2023-11-23 22:06:25 -0500

    register groups models with admin site

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Expense, ExpenseGroup, ExpenseGroupUser, ExpenseSplit
+
+admin.site.register(ExpenseGroup)
+admin.site.register(ExpenseGroupUser)
+admin.site.register(Expense)
+admin.site.register(ExpenseSplit)
